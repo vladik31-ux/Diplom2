ac511879a0f29f98742cf01ea2c13f4d4267bfdf
vladik31-ux <vladik200131@bk.ru>
2020-12-20 00:01:42 +0500

    qwe

--- app/src/main/java/com/example/diplom/LoginActivity.java
+++ app/src/main/java/com/example/diplom/LoginActivity.java
@@ -1,25 +1,38 @@
 package com.example.diplom;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText login, password;
     private Drawable img, img2;
     private String P, L;
+    private FirebaseAuth mAuth;
 
     @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
 
         login = findViewById(R.id.login);
         password = findViewById(R.id.password);
@@ -31,16 +44,42 @@ public class LoginActivity extends AppCompatActivity {
         img2 = this.getResources().getDrawable(R.drawable.key);
         img2.setBounds(0, 0, 60, 60);
         password.setCompoundDrawables(img2, null, null, null);
+
+
     }
 
     public void enter(View view) {
-        P = login.toString();
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        L = login.getText().toString();
+        P = password.getText().toString();
+        if(!L.isEmpty() && !P.isEmpty()) {
+            mAuth.signInWithEmailAndPassword(L, P)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(LoginActivity.this, "Неправильный логин или пароль!",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+        } else {
+            Toast.makeText(LoginActivity.this, "Заполните пожалуйста все поля",
+                    Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     public void registr(View view) {
         Intent intent = new Intent(this, RegistrationActivity.class);
         startActivity(intent);
     }
+
+
 }
--- app/src/main/java/com/example/diplom/RegistrationActivity.java
+++ app/src/main/java/com/example/diplom/RegistrationActivity.java
@@ -1,15 +1,28 @@
 package com.example.diplom;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private EditText login, password, name, password2;
     private Drawable img, img2;
+    private String P, P2, N, L;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +34,63 @@ public class RegistrationActivity extends AppCompatActivity {
         name = findViewById(R.id.name);
         password2 = findViewById(R.id.password2);
 
+        mAuth = FirebaseAuth.getInstance();
+
         img = this.getResources().getDrawable(R.drawable.iconmail);
         img.setBounds(0, 0, 60, 60);
         login.setCompoundDrawables(img, null, null, null);
-        password.setCompoundDrawables(img, null, null, null);
+        name .setCompoundDrawables(img, null, null, null);
 
         img2 = this.getResources().getDrawable(R.drawable.key);
         img2.setBounds(0, 0, 60, 60);
         password.setCompoundDrawables(img2, null, null, null);
-        password.setCompoundDrawables(img2, null, null, null);
+        password2.setCompoundDrawables(img2, null, null, null);
+    }
+
+    public void registr(View view) {
+        L = login.getText().toString();
+        P = password.getText().toString();
+        N = name.getText().toString();
+        P2 = password2.getText().toString();
+
+        if(!L.isEmpty() && !P.isEmpty() && !N.isEmpty() && !P2.isEmpty()) {
+            if (isEmailValid(L)) {
+                if(P.equals(P2)) {
+                    mAuth.createUserWithEmailAndPassword(L, P)
+                            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+
+                                        Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+
+                                    } else {
+                                        if(P.length() < 4 && P2.length() < 4) {
+                                            Toast.makeText(RegistrationActivity.this, "Слишком короткий пароль",
+                                                    Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            Toast.makeText(RegistrationActivity.this, "Ошибка регистрации, попробуйте еще раз",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                }
+                            });
+                } else {
+                    Toast.makeText(RegistrationActivity.this, "Пароли не совпадают",
+                            Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(RegistrationActivity.this, "Введен не корректо email",
+                    Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(RegistrationActivity.this, "Заполните пожалуйста все поля",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean isEmailValid(String email){
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 }
